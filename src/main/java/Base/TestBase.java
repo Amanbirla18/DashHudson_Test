84cdb14c2376e905c98d0b4f8922fe766232a48e
Aman Birla <amanbirla18@gmail.com>
2020-09-12 22:01:21 -0400

    changes

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-//import testutils.TestUtil;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import testutils.TestUtil;
